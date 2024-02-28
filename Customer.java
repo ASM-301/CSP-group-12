8c16b07ac4129f425a64c9782f604c1f076cec41
ASM-301 <161630885+ASM-301@users.noreply.github.com>
2024-02-28 20:21:07 +0300

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,26 @@
+
+
+	public class Customer {
+
+
+
+		    int demand;
+		    int timeWindowStart;
+		    int timeWindowEnd;
+
+		    public Customer(int demand, int timeWindowStart, int timeWindowEnd) {
+		        this.demand = demand;
+		        this.timeWindowStart = timeWindowStart;
+		        this.timeWindowEnd = timeWindowEnd;
+		    }
+		}
+	
+	
+	
+	
+
+		
+		
+		
+	
+
